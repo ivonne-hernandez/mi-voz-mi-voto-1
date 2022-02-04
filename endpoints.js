dec42f5562b82c2134c888c392cb6cc19ee21ba8
Elise Beall <elisebeall@gmail.com>
2022-02-03 17:25:39 -0700

    Add skeleton endpoints.js file for JSON contract

---
+++ endpoints.js
@@ -0,0 +1,13 @@
+const endpoints = {
+  submit: ''
+  /* POST to API expects this request object:
+    {
+      firstName: 'string',
+      email: 'string',
+      zipCode: 'num',
+      lang: 'string' ** 'en' or 'es' **
+    }
+  */
+}
+
+export default endpoints;
