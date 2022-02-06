106e031c2d6c6759aa035480197253463ee79c61
Elise Beall <elisebeall@gmail.com>
2022-02-05 22:18:16 -0700

    Create boilerplate devs array

---
+++ src/Assets/devs.js
@@ -0,0 +1,36 @@
+const devs = [
+  {
+    name: 'Elise Beall',
+    photo: '',
+    title: 'Front-End Software Developer',
+    github: 'https://github.com/elisebeall',
+    linkedin: 'https://www.linkedin.com/in/elisejbeall',
+    bio: ''
+  },
+  {
+    name: 'Ivonne Hernandez',
+    photo: '',
+    title: 'Front-End Software Developer',
+    github: 'https://github.com/ivonne-hernandez',
+    linkedin: 'https://www.linkedin.com/in/ivonne-hernandez-107b0324/',
+    bio: ''
+  },
+  {
+    name: 'Alex McConnell',
+    photo: '',
+    title: 'Back-End Software Developer',
+    github: 'https://github.com/AlexMMcConnell',
+    linkedin: 'https://www.linkedin.com/in/ivonne-hernandez-107b0324/',
+    bio: ''
+  },
+  {
+    name: 'Luis Arroyo',
+    photo: '',
+    title: 'Back-End Software Developer',
+    github: 'https://github.com/larroyo1',
+    linkedin: 'https://www.linkedin.com/in/luis-arroyo-65a954181/',
+    bio: ''
+  }
+]
+
+export default devs;
