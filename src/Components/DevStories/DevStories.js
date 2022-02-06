5f7f8334d0790a6e1cd4b4825d71834f373fa342
Elise Beall <elisebeall@gmail.com>
2022-02-05 22:19:26 -0700

    Write skeleton structure for DevStories component

---
+++ src/Components/DevStories/DevStories.js
@@ -0,0 +1,27 @@
+import { Fragment } from 'react';
+import devs from '../Assets/devs.js';
+
+const DevStories = () => {
+  const devStory = devs.map(dev => {
+    return (
+      <>
+        <h2 className="dev-name">{dev.name}</h2>
+        <h3 className="dev-title">{dev.title}</h3>
+        <img className="dev-photo" src={dev.photo} alt={dev.name} />
+        <a className="dev-linkedin" href={dev.linkedin}>linkedin</a>
+        <a className="dev-github" href={dev.github}>github</a>
+        <p className="dev-bio">{dev.bio}</p>
+      </>
+    )
+  })
+
+  return(
+    <>
+      <h1>The Story Behind 'Mi Voz, Mi Voto'</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum dapibus enim at sollicitudin. Proin ac mattis risus. Maecenas iaculis eros vitae enim semper, at egestas massa mattis. Maecenas mattis pharetra magna, viverra sollicitudin tellus pharetra quis. Vestibulum tincidunt congue arcu in laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac malesuada sapien. Aliquam consectetur felis semper velit ullamcorper, sit amet porttitor sem semper. Duis vehicula et nisi vel tempus. Suspendisse nunc diam, porta vulputate ultrices quis, consequat eu leo. Proin nisi quam, semper nec arcu in, semper interdum magna. Cras aliquet fermentum lacus vel posuere.</p>
+      {devStory}
+    </>
+  )
+}
+
+export default DevStories;
