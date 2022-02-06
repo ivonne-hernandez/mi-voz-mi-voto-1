17e6e7dedbdf84b436630490b9a79b9515dd5b0c
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-06 09:18:51 -0700

    Removed the states array into it's own states.js file and removed the wireframe postNewEmailSubcriber prop

--- src/Components/EmailNotificationForm/EmailNotificationForm.js
+++ src/Components/EmailNotificationForm/EmailNotificationForm.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import "./EmailNotificationForm.css";
+import { states } from "./states"
 
 class EmailNotificationForm extends Component {
   constructor() {
@@ -36,7 +37,7 @@ class EmailNotificationForm extends Component {
         email: this.state.email,
         language: this.state.language,
       }
-      this.props.postNewEmailSubscriber(newEmailSubscriber);
+      // this.props.postNewEmailSubscriber(newEmailSubscriber);
       this.clearInputs();
     }
   }
@@ -92,60 +93,6 @@ class EmailNotificationForm extends Component {
   }
 
   stateOptions = () => {
-    const states = [
-      { value: "al", label: "Alabama" },
-      { value: "ak", label: "Alaska" },
-      { value: "az", label: "Arizona" },
-      { value: "ar", label: "Arkansas" },
-      { value: "ca", label: "California" },
-      { value: "co", label: "Colorado" },
-      { value: "ct", label: "Connecticut" },
-      { value: "de", label: "Delaware" },
-      { value: "dc", label: "District Of Columbia" },
-      { value: "fl", label: "Florida" },
-      { value: "ga", label: "Georgia" },
-      { value: "hi", label: "Hawaii" },
-      { value: "id", label: "Idaho" },
-      { value: "il", label: "Illinois" },
-      { value: "in", label: "Indiana" },
-      { value: "ia", label: "Iowa" },
-      { value: "ks", label: "Kansas" },
-      { value: "ky", label: "Kentucky" },
-      { value: "la", label: "Louisiana" },
-      { value: "me", label: "Maine" },
-      { value: "md", label: "Maryland" },
-      { value: "ma", label: "Massachusetts" },
-      { value: "mi", label: "Michigan" },
-      { value: "mn", label: "Minnesota" },
-      { value: "ms", label: "Mississippi" },
-      { value: "mo", label: "Missouri" },
-      { value: "mt", label: "Montana" },
-      { value: "ne", label: "Nebraska" },
-      { value: "nv", label: "Nevada" },
-      { value: "nh", label: "New Hampshire" },
-      { value: "nj", label: "New Jersey" },
-      { value: "nm", label: "New Mexico" },
-      { value: "ny", label: "New York" },
-      { value: "nc", label: "North Carolina" },
-      { value: "nd", label: "North Dakota" },
-      { value: "oh", label: "Ohio" },
-      { value: "ok", label: "Oklahoma" },
-      { value: "or", label: "Oregon" },
-      { value: "pa", label: "Pennsylvania" },
-      { value: "ri", label: "Rhode Island" },
-      { value: "sc", label: "South Carolina" },
-      { value: "sd", label: "South Dakota" },
-      { value: "tn", label: "Tennessee" },
-      { value: "tx", label: "Texas" },
-      { value: "ut", label: "Utah" },
-      { value: "vt", label: "Vermont" },
-      { value: "va", label: "Virginia" },
-      { value: "wa", label: "Washington" },
-      { value: "wv", label: "West Virginia" },
-      { value: "wi", label: "Wisconsin" },
-      { value: "wy", label: "Wyoming" }
-    ];
-
     const options = [
       <option className="state-name-option" name="state_name" value="Select" key="Select" disabled={true}>
         Select
