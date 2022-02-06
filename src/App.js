919932152e6a768a1c7be7ada8c25370475ef06f
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-06 09:19:25 -0700

    Removed the wireframe postNewEmailSubscriber proper

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <MainContainer postNewEmailSubscriber={this.postNewEmailSubscriber}/>
+        <MainContainer />
       </div>
     );
   }
