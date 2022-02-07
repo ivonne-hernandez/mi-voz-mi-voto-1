92c4a8ae012f0542b036520078fb4cf86550b3f9
Elise Beall <elisebeall@gmail.com>
2022-02-07 16:28:12 -0700

    Import apiCalls into form for POST to BE api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
-import { Component } from "react";
-import Header from "./Components/Header/Header";
-import MainContainer from "./Components/MainContainer/MainContainer";
+import './App.css';
+import { Component } from 'react';
+import Header from './Components/Header/Header';
+import MainContainer from './Components/MainContainer/MainContainer';
 
-class App extends Component {  
+class App extends Component {
   render = () => {
     return (
       <div className="App">
