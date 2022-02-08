6c0e52b733819f25d84d5cf3afbbde5455600f1d
Elise Beall <elisebeall@gmail.com>
2022-02-07 18:00:53 -0700

    Update endpoints to reflect changed messaging on POST response

--- src/endpoints.js
+++ src/endpoints.js
@@ -3,17 +3,37 @@ const endpoints = {
   /* POST to API:
     request object:
       {
-        firstName: 'string',
+        first_name: 'string',
+        last_name: 'string',
+        state_name: 'string', ** 'co' or 'ca' **
         email: 'string',
-        zipCode: 'num',
-        lang: 'string' ** 'en' or 'es' **
+        language: 'string' ** 'en' or 'es' **
       }
     response object:
-    -> WHAT DO WE WANT THE BACKEND TO RETURN UPON A SUCCESSFUL POST?
-      success: "New user successfully created.  A confimation has been sent to ${email}.",
-      status: "200",
-      error: "Unable to create user.  Please provide a valid email."
+      SUCCESSFULLY CREATED:
+      {
+        success: "You are now registered to receive notifications about upcoming elections in your state. A confirmation email has been sent to #{user.email}.",
+        status: 200
+      }
+      EMAIL DOES NOT INCLUDE '@':
+      {
+        error: "Error creating subscriber. A valid email must be provided."},
+        status: 400
+      }
+      GENERAL ERROR:
+      {
+        error: "Error creating subscriber.",
+        status: 400
+      }
+      SUCCESSFULLY REMOVED:
+      {
+        success: "You have successfully unsubscribed from Mi Voto, Mi Voz's email list. You will no longer receive email notifications at #{user.email}."},
+        status: 200
+      }
   */
+  elections: 'http://localhost:3001/api/v1/elections',
+  referendums: 'http://localhost:3001/api/v1/referendums',
+  state: 'http://localhost:3001/api/v1/state'
 }
 
 export default endpoints;
