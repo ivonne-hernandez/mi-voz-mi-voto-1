c8eb3d6530729e169e98b33137d96492872838d9
Elise Beall <elisebeall@gmail.com>
2022-02-07 19:49:54 -0700

    Add error container div

--- src/Components/Error/Error.js
+++ src/Components/Error/Error.js
@@ -1,11 +1,15 @@
 import { Fragment } from 'react';
 import './Error.css';
+import noVoice1 from '../../Assets/no-voice1.png';
+import noVoice2 from '../../Assets/no-voice2.png';
 
 const Error = ({ error }) => {
   return (
-    <>
-      {error}
-    </>
+    <div className="error-container">
+      <img className="error-image" src={noVoice1}
+        alt="microphone with horizontal strikethrough" />
+      <h3 className="error-text">{error}</h3>
+    </div>
   )
 }
 
