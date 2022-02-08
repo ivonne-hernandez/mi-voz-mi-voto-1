0a1c063ed6e2e1658659192cd3513cd75424b053
Elise Beall <elisebeall@gmail.com>
2022-02-07 19:49:09 -0700

    Remove destructuring of request object

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,10 @@
 import endpoints from './endpoints.js';
 
 export const postNewEmailSubscriber = (newEmailSubscriber) => {
-  const { first_name, last_name, state_name, email, language } = newEmailSubscriber;
-
   return fetch(endpoints.submit, {
     mode: 'cors',
     method: 'POST',
     headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
-    body: JSON.stringify({
-      first_name: first_name,
-      last_name: last_name,
-      state_name: state_name,
-      email: email,
-      language: language
-    })
+    body: JSON.stringify(newEmailSubscriber)
   })
 }
