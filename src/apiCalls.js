733361c6bcffdf99d91dc3c6cae23a75c861dfbf
Elise Beall <elisebeall@gmail.com>
2022-02-07 20:21:09 -0700

    Remove destructuring of newEmailSubscriber from apiCalls.js

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,18 +1,13 @@
 import endpoints from './endpoints.js';
 
 export const postNewEmailSubscriber = (newEmailSubscriber) => {
-  const { first_name, last_name, state_name, email, language } = newEmailSubscriber;
-
   return fetch(endpoints.submit, {
     mode: 'cors',
     method: 'POST',
-    headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
-    body: JSON.stringify({
-      first_name: first_name,
-      last_name: last_name,
-      state_name: state_name,
-      email: email,
-      language: language
-    })
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(newEmailSubscriber)
   })
 }
