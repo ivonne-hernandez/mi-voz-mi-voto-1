200063450a71bd96f03af9189d07e42b5b69eb5f
Elise Beall <elisebeall@gmail.com>
2022-02-07 19:51:17 -0700

    Add loading container div

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,11 +1,12 @@
 import { Fragment } from 'react';
 import './Loading.css';
+import loading from '../../Assets/loading.png';
 
 const Loading = () => {
   return (
-    <>
-      LOADING...
-    </>
+    <div className="loading-icon-container">
+      <img className="loading-icon" src={loading} alt="loading icon" />
+    </div>
   )
 }
 
