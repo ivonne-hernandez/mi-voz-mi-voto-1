4768bb804d68ae77dbe5590bbb96957be8bdb6d1
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-09 14:25:24 -0700

    Add react router v6 and create home, get-notifications and register-to-vote paths

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import MainContainer from './Components/MainContainer/MainContainer';
+import HomePageRoutingComponent from './Components/HomePageRoutingComponent/HomePageRoutingComponent';
+import RegisterToVote from './Components/RegisterToVote/RegisterToVote';
 
 class App extends Component {
   render = () => {
     return (
-      <div className="App">
+      <main className="App">
         <Header />
-        <MainContainer />
-      </div>
+        <Routes>
+          <Route path="/get-notifications" element={<MainContainer />}/>
+          <Route path="/register-to-vote" element={<RegisterToVote />}/>
+          <Route path="/" element={<HomePageRoutingComponent />}/>
+        </Routes>
+      </main>
+
     );
   }
 }
