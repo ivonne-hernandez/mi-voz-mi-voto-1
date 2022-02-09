ed5e003724ba64291b05df7779af45543270a17d
Elise Beall <elisebeall@gmail.com>
2022-02-08 22:37:28 -0700

    Rename spec to combine flows for form & error handling; add testing for typing in inputs

---
+++ cypress/integration/email-notification-form-spec.js
@@ -0,0 +1,112 @@
+describe('Mi Voz, Mi Voto email notification form user flow', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  });
+
+  it('Should be able to input a first name, last name, email & select a state, language, and confirm subscription.', () => {
+    cy.fixture('user1.json').as('user1').then((user1) => {
+      cy.get('input[id=first_name]').type(user1.first_name)
+        .get(`input[value=${user1.first_name}]`).should('equal', `${user1.first_name}`)
+        .get('input[id=last_name]').type(user1.last_name)
+        .get(`input[value=${user1.last_name}]`).should('equal', `${user1.last_name}`)
+        .get('select[id=state_name]').select(user1.state_name)
+        .get(`select[value=${user1.state_name}]`).should('equal', `${user1.state_name}`)
+        .get('input[id=email]').type(user1.email)
+        .get(`input[value=${user1.email}]`).should('equal', `${user1.email}`)
+        .get('input[id=english]').click()
+        .get(`input[value=${user1.language}]`).should('equal', `${user1.language}`)
+        .get('input[id=agree_to_emails]').should('equal', 'false')
+        .get('input[id=agree_to_emails]').click()
+        .get('input[id=agree_to_emails]').should('equal', 'true')
+    })
+  });
+
+  it('Should include text-based labels for each input', () => {
+    cy.get('form[class="form-container"]').should('have.length', 1)
+      .get('p[class="form-header"]').contains('State Election Reminders')
+      .get('label[class="label"]').contains('First Name')
+      .get('label[class="label"]').contains('Last Name')
+      .get('label[class="label"]').contains('State')
+      .get('label[class="label"]').contains('Email address')
+      .get('label[class="preferred-lang-p label"]').contains('Preferred language')
+      .get('label[class="label-radio"]').contains('English')
+      .get('label[class="label-radio"]').contains('Spanish')
+      .get('label[class="agree-to-emails-checkbox label"]').contains('Sign up for email notifications about upcoming elections in my state.')
+      .get('button[class="submit-button"]').should('contain', 'Submit')
+  });
+
+  
+
+  it('Should display a loading image while the submitting the form', () => {
+    cy.fixture('user1.json').as('user1').then((user1) => {
+      cy.get('input[id=first_name]').type(user1.first_name)
+        .get('input[id=last_name]').type(user1.last_name)
+        .get('select[id=state_name]').select(user1.state_name)
+        .get('input[id=email]').type(user1.email)
+        .get('input[id=english]').click()
+        .get('input[id=agree_to_emails]').click()
+        .get('.submit-button').click()
+        .intercept('POST', 'http://localhost:3001/api/v1/users', {
+        body: user1
+      })
+    })
+      .get('.loading-icon').should('be.visible')
+  });
+
+  it('Should display an error message if the email is already subscribed', () => {
+    cy.fixture('user1.json').as('user1').then((user1) => {
+      cy.get('input[id=first_name]').type(user1.first_name)
+        .get('input[id=last_name]').type(user1.last_name)
+        .get('select[id=state_name]').select(user1.state_name)
+        .get('input[id=email]').type(user1.email)
+        .get('input[id=english]').click()
+        .get('input[id=agree_to_emails]').click()
+        .get('.submit-button').click()
+        .intercept('POST', 'http://localhost:3001/api/v1/users', {
+        body: user1
+      })
+    })
+      .get('.error-text').should('contain', 'This email is already susbscribed to receive election notifications.')
+  })
+
+  it('Should display a message confirming the subscription to election notifications', () => {
+    cy.fixture('user2.json').as('user2').then((user2) => {
+      cy.get('input[id=first_name]').type(user2.first_name)
+        .get('input[id=last_name]').type(user2.last_name)
+        .get('select[id=state_name]').select(user2.state_name)
+        .get('input[id=email]').type(user2.email)
+        .get('input[id=english]').click()
+        .get('input[id=agree_to_emails]').click()
+        .get('.submit-button').click()
+        .intercept('POST', 'http://localhost:3001/api/v1/users', {
+        body: user2
+      })
+      .get('.success-message').should('contain', `You are now registered to receive notifications about upcoming elections in your state. A confirmation email has been sent to ${user2.email}.`)
+    })
+  })
+
+  it('Should display an error image & error message if the server can\'t complete the request', () => {
+    cy.fixture('user1.json').as('user1').then((user1) => {
+        .get('input[id=first_name]').type(user1.first_name)
+        .get('input[id=last_name]').type(user1.last_name)
+        .get('select[id=state_name]').select(user1.state_name)
+        .get('input[id=email]').type(user1.email)
+        .get('input[id=english]').click()
+        .get('input[id=agree_to_emails]').click()
+        .get('.submit-button').click()
+        .intercept('POST', 'http://localhost:3001/api/v1/users', {
+        body: {"first_name": "Elise"}
+      })
+    })
+    cy.get('.error-text').should('contain', 'Error creating subscriber.')
+  })
+
+  it.skip('Should display a button to send the user back to the form', () => {
+
+  })
+
+  it.skip('', () => {
+
+  })
+
+})
