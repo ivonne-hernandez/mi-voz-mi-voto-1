893b44337aefa4a64e866680a47a1cc56ee66cae
Elise Beall <elisebeall@gmail.com>
2022-02-11 02:51:47 -0700

    Rearrange dev order so I can go last

--- src/Assets/devs.js
+++ src/Assets/devs.js
@@ -1,35 +1,40 @@
+import elise from './placeholder.jpeg';
+import ivonne from './placeholder.jpeg';
+import alex from './placeholder.jpeg';
+import luis from './placeholder.jpeg';
+
 const devs = [
   {
-    name: 'Elise Beall',
-    photo: '',
-    title: 'Front-End Software Developer',
-    github: 'https://github.com/elisebeall',
-    linkedin: 'https://www.linkedin.com/in/elisejbeall',
-    bio: '"As a former volunteer coordinator & field organizer of local & national political campaigns, I have seen first hand the divide created by language barriers & technical challenges.  Working on this application allowed me to focus on creating a tool to help level the playing field of electoral politics."'
-  },
-  {
     name: 'Ivonne Hernandez',
-    photo: '',
+    photo: ivonne,
     title: 'Front-End Software Developer',
     github: 'https://github.com/ivonne-hernandez',
     linkedin: 'https://www.linkedin.com/in/ivonne-hernandez-107b0324/',
-    bio: '"The goal of this application is to remove the language barrier that keeps a lot of Latinos from showing up to the polls. It’s meant to empower the Latino community by providing election resources and information in a language they know and understand so that they too, can have their voices be heard through their vote."'
+    story: '"The goal of this application is to remove the language barrier that keeps a lot of Latinos from showing up to the polls. It’s meant to empower the Latino community by providing election resources and information in a language they know and understand so that they too, can have their voices be heard through their vote."'
   },
   {
     name: 'Alex McConnell',
-    photo: '',
+    photo: alex,
     title: 'Back-End Software Developer',
     github: 'https://github.com/AlexMMcConnell',
     linkedin: 'https://www.linkedin.com/in/alex-m-mcconnell/',
-    bio: '"Voter empowerment has always been something important to me as larger companies have been continually taking over the entire political hemisphere. This project is another opportunity to do just that; empower voters who may not know how to use their voices."'
+    story: '"Voter empowerment has always been something important to me as larger companies have been continually taking over the entire political hemisphere. This project is another opportunity to do just that; empower voters who may not know how to use their voices."'
   },
   {
     name: 'Luis Arroyo',
-    photo: '',
+    photo: luis,
     title: 'Back-End Software Developer',
     github: 'https://github.com/larroyo1',
     linkedin: 'https://www.linkedin.com/in/luis-arroyo-65a954181/',
-    bio: '"Being a Latino myself, I wanted to help create a tool that encourages and helps people in my family to get out and vote."'
+    story: '"Being a Latino myself, I wanted to help create a tool that encourages and helps people in my family to get out and vote."'
+  },
+  {
+    name: 'Elise Beall',
+    photo: elise,
+    title: 'Front-End Software Developer',
+    github: 'https://github.com/elisebeall',
+    linkedin: 'https://www.linkedin.com/in/elisejbeall',
+    story: '"As a former volunteer coordinator & field organizer of local & national political campaigns, I have seen first hand the divide created by language barriers & technical challenges.  Working on this application allowed me to focus on creating a tool to help level the playing field of electoral politics."'
   }
 ]
 
