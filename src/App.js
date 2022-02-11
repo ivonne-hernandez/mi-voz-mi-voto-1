75451dfa79271588d1083df45e904410006708b9
Elise Beall <elisebeall@gmail.com>
2022-02-11 00:15:07 -0700

    Add routes for DevStories & Main

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
 import { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import MainContainer from './Components/MainContainer/MainContainer';
+import DevStories from './Components/DevStories/DevStories';
 
 class App extends Component {
   render = () => {
     return (
       <div className="App">
         <Header />
-        <MainContainer />
+        <Routes>
+          <Route path="/" element={<MainContainer />} />
+          <Route path="/about" element={<DevStories />} />
+        </Routes>
       </div>
     );
   }
