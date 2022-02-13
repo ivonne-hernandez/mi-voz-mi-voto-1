08c398a2b16fe126f5aed63cd12887952462bed7
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-13 09:30:36 -0700

    Refactor the img code to a single line and wrap the app's name in a FormattedMessage component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,13 +6,12 @@ import './Header.css';
 const Header = ()  => {
   return (
     <header className="header-container">
-      <img
-        className="vote-image"
-        src={voteImage}
-        alt="vote"/>
-      <Link className="app-name" 
-        to="/">
-        My Voice, My Vote
+      <img className="vote-image" src={voteImage} alt="vote"/>
+      <Link className="app-name" to="/">
+        <FormattedMessage 
+          id="header.appName"
+          defaultMessage="My Voice, My Vote"
+        />
       </Link>
       <button
         className="our-story-button header-button">
