d81f14eee6e9f7d2c28fb0717a7528a7ae27b89d
Elise Beall <elisebeall@gmail.com>
2022-02-13 04:47:08 -0700

    Move ul tag to main render, change route-card-header to h2 tag

--- src/Components/RouteCard/RouteCard.js
+++ src/Components/RouteCard/RouteCard.js
@@ -4,19 +4,19 @@ import './RouteCard.css';
 const RouteCard = ({ header, listItems, linksTo }) => {
   const linkTags = listItems.map((link, index) => {
     return (
-      <ul className="link-list" key={link}>
-        <li>
-          <Link to={linksTo[index]} className="link-item">{link}</Link>
-        </li>
-      </ul>
+      <li key={link}>
+        <Link to={linksTo[index]} className="link-item">{link}</Link>
+      </li>
     );
   });
 
   return (
     <article className={`route-card-article ${header.toLowerCase().split(' ').join('-')}`}>
       <div className="route-card-container">
-        <p className="route-card-header">{header}</p>
-        {linkTags}
+        <h2 className="route-card-header">{header}</h2>
+        <ul className="link-list">
+          {linkTags}
+        </ul>
       </div>
     </article>
   );
