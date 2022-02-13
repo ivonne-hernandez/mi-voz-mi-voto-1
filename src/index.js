9eecdea5f3d19a9d71b729686619a5f12dd70a7f
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-13 09:25:02 -0700

    Import the en and es language files into index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
+import Spanish from './Languages/es.json';
+import English from './Languages/en.json';
 
 ReactDOM.render(
   <BrowserRouter>
