eb4ec75c61fca3c231ac91abadaa4f489de1714e
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-13 16:57:03 -0700

    Wrap remaining list items from the ElectionProtection in a FormattedMessage and add properties for the list items to the en/es files

--- src/Components/ElectionProtection/ElectionProtection.js
+++ src/Components/ElectionProtection/ElectionProtection.js
@@ -39,10 +39,14 @@ const ElectionProtection = () => {
               defaultMessage="Try your best to keep your place in line. You are allowed to cast your ballot as long as you're in line before the polls close, but if you leave the line you may not be allowed to vote upon your return." /> 
           </li>
           <li className="list-item">
-            Try your best to vote on a regular ballot. Only cast a provisional ballot if you don't have any other option. Unfortunately, many states will not count a provisional ballot if it was casted at the wrong polling location.
+            <FormattedMessage
+              id="electionProtection.listItem4"
+              defaultMessage="Try your best to vote on a regular ballot. Only cast a provisional ballot if you don't have any other option. Unfortunately, many states will not count a provisional ballot if it was casted at the wrong polling location." />
           </li>
           <li className="list-item">
-            If any issues arise at the polling site do your best to resolve them then and there. Keep in mind that most poll workers are genuinely trying their best, but they're human too and humans sometimes make mistakes.
+            <FormattedMessage
+              id="electionProtection.listItem5"
+              defaultMessage="If any issues arise at the polling site do your best to resolve them then and there. Keep in mind that most poll workers are genuinely trying their best, but they're human too and humans sometimes make mistakes." />
           </li>
         </ol>
       </article>
