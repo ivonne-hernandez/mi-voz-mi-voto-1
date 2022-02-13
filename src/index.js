1f911c2915e1b50b308c45e62a489b8193845aa0
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-13 09:22:19 -0700

    Import IntlProvider from react-intl to index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 
 ReactDOM.render(
   <BrowserRouter>
