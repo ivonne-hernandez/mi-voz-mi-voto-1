62bb4e520aabdb6fc1c2c14ce2dc2215176a844b
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-13 10:24:46 -0700

    Remove the routes from the HomePageRouting component into a separate file and import that file

--- src/Components/HomePageRouting/HomePageRouting.js
+++ src/Components/HomePageRouting/HomePageRouting.js
@@ -1,15 +1,9 @@
 import { FormattedMessage } from 'react-intl';
 import RouteCard from '../RouteCard/RouteCard';
 import './HomePageRouting.css';
+import routes from './routes.js';
 
 const HomePageRoutingComponent = () => {
-  const routes = [
-    { header: 'Vote', listItems: ['Register to vote', 'Check my voter registration'], linksTo: ['/register-to-vote', '/check-my-registration'] },
-    { header: 'Know My Rights', listItems: ['Find out about Election Protection'], linksTo: ['/election-protection']},
-    { header: 'Know What Forms of ID I\'ll Need', listItems:['Find out about Voter ID Laws'], linksTo: ['/voter-id-laws']},
-    { header: 'Get Notifications', listItems:['Get notifications about upcoming elections in my state'], linksTo: ['/get-notifications']}
-  ];
-
   const displayRouteCards = () => {
     const routeCards = routes.map((route, index) => {
       return (
