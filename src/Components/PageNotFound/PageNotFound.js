7b31b4c909e883a091041a88aaca7f94c6cca087
Elise Beall <elisebeall@gmail.com>
2022-02-13 17:19:17 -0700

    Refactor to use 404 icon instead of placeholder; fix styling on bttn

--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -1,13 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import './PageNotFound.css';
-import notFound from '../../Assets/placeholder.jpeg';
+import notFound from '../../Assets/404-error.png';
 
 const PageNotFound = () => {
   const navigate = useNavigate();
 
   return (
     <div className="page-not-found-container">
-      <img src={notFound} alt="error" />
+      <img src={notFound} alt="white sign with blue header and text reading 404" />
       <h2>Sorry, we can't seem to find the page you're looking for, please try again.</h2>
       <button onClick={() => navigate('/')}>Home</button>
     </div>
