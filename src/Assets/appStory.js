55a696b8a320d525508e99bd5b654e95ffbf4034
Elise Beall <elisebeall@gmail.com>
2022-02-13 17:20:25 -0700

    Delete unnecessary file

--- src/Assets/appStory.js
+++
@@ -1,6 +0,0 @@
-const appStory = {
-  title: "The Story Behind 'Mi Voz, Mi Voto'",
-  text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum dapibus enim at sollicitudin. Proin ac mattis risus. Maecenas iaculis eros vitae enim semper, at egestas massa mattis. Maecenas mattis pharetra magna, viverra sollicitudin tellus pharetra quis. Vestibulum tincidunt congue arcu in laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac malesuada sapien. Aliquam consectetur felis semper velit ullamcorper, sit amet porttitor sem semper. Duis vehicula et nisi vel tempus. Suspendisse nunc diam, porta vulputate ultrices quis, consequat eu leo. Proin nisi quam, semper nec arcu in, semper interdum magna. Cras aliquet fermentum lacus vel posuere.'
-};
-
-export default appStory;
