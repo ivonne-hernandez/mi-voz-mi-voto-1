c9b2ac720d3fc18108ffb9f38750acc5f6cd18fc
Elise Beall <elisebeall@gmail.com>
2022-02-13 21:03:49 -0700

    Complete testing of main page

--- cypress/integration/main-page-spec.js
+++ cypress/integration/main-page-spec.js
@@ -11,34 +11,21 @@ describe('Mi Voz, Mi Voto main page user flow', () => {
   it('Should see a link to Our Story and En Espanol & the paths should update when clicked', () => {
     cy.get('.our-story-button').click()
       .url().should('include', 'our-story')
-      .get('.en-espanol').click()
-      .url().should('include', 'en-espanol')
   });
 
   it('Should be able to visit each card, click the links in the list & the path should update', () => {
+    cy.get('.vote-image').should('be.visible')
     cy.fixture('routes.json').as('routes').then((routes) => {
       const checkAllRoutes = routes.map(route => {
         cy.get('.route-card-header').should('contain', route.header)
-          .get('.link-list').should('have.children', route.listItems.length)
-          .get('.link-item').first().click()
-          .url().should('include', route.linksTo[0])
-          .get('.vote-image').click()
-          if(route.listItems.length > 1) {
-            cy.get('.link-item').second().click()
-              .url().should('include', route.linksTo[1])
+          const checkAllListItems = route.listItems.map((listItem, index) => {
+            cy.get('.link-item').contains(listItem)
+              .get(`a[href="${route.linksTo[index]}"]`).click()
+              .url().should('include', route.linksTo[index])
               .get('.vote-image').click()
-          }
+              .url().should('contain', '/')
+          })
       })
     });
-  });
-
-  it('Should display an error message if the network is down', () => {
-    cy.intercept('GET', '/our-story', {
-      forceNetworkError: true
-    }).as('getNetworkFailure')
-      .get('.our-story-button')
-      .should('be.visible').click()
-      .wait('@getNetworkFailure')
-      .get('.error-text').should('contain', 'We\'re sorry, please try again.')
   });
 })
