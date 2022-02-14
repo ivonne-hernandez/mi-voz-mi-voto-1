65cac4c702e4544bbdc91971551f9cf05518e064
Elise Beall <elisebeall@gmail.com>
2022-02-14 14:20:44 -0700

    Add testing for external links

--- cypress/integration/election-protection-spec.js
+++ cypress/integration/election-protection-spec.js
@@ -1,15 +1,15 @@
-describe('Voter Id Laws page user flow', () => {
+describe('Election Protection page user flow', () => {
   beforeEach(() => {
-    cy.checkPageA11y('/voter-id-laws')
+    cy.checkPageA11y('/election-protection')
   });
 
   it('Should display voter ID laws', () => {
-    cy.fixture('voterIdLaws.json').as('voterIdLaws').then((voterIdLaws) => {
-      const { header2, header3, listItems } = voterIdLaws;
+    cy.fixture('electionProtection.json').as('electionProtection').then((electionProtection) => {
+      const { header2, header3, listItems } = electionProtection;
       cy.get('h2').should('contain', header2)
         .get('h3').should('contain', header3)
         const checkListItems = listItems.map((listItem, index) => {
-          cy.get('ul')
+          cy.get('ol')
             .get('li').eq(index).should('contain', listItem[index])
         })
     })
@@ -25,7 +25,7 @@ describe('Voter Id Laws page user flow', () => {
   });
 
   it('Should have a matching route in the URL', () => {
-    cy.url().should('contain', '/voter-id-laws')
+    cy.url().should('contain', '/election-protection')
   });
 
   it('Should be able to navigate back to the main page', () => {
