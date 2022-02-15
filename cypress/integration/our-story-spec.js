ce8501cd2de5777595c30b57d5aa3cba5a5ae903
Elise Beall <elisebeall@gmail.com>
2022-02-15 15:31:59 -0700

    Complete refactor of tests to check translations

--- cypress/integration/our-story-spec.js
+++ cypress/integration/our-story-spec.js
@@ -1,19 +1,42 @@
 describe('Our Story page user flow', () => {
   beforeEach(() => {
     cy.checkPageA11y('/our-story')
+    cy.get('.en-espanol-button').select('English')
   });
 
-  it('Should see all four developers\' names, photo, title, and reason for working on the app', () => {
-    cy.fixture('devs.json').as('devs').then((devs) => {
-      const checkAllDevs = devs.map(dev => {
-      cy.get('.dev-name').should('contain', dev.name)
-        .get(`.${dev.name.split(' ')[0]}`).should('be.visible')
-        .get(`.${dev.name.split(' ')[0]}`).should('have.attr', 'alt')
-        .should('contain', dev.name)
-        .get('.dev-title').should('contain', dev.title)
-        .get('.dev-story').should('contain', dev.story)
+  it('Should see all four devs\' names, photo, title, & reason for working on the app, in English & Spanish, ', () => {
+    cy.fixture('english.json').as('english').then((english) => {
+      cy.fixture('devs.json').as('devData').then((devData) => {
+        const checkAllDevData = devData.map(developer => {
+          const checkAllDevTranslations = english.devs.map(dev => {
+            cy.get('.dev-name').should('contain', developer.name)
+              .get(`.${developer.name.split(' ')[0]}`).should('be.visible')
+              .get(`.${developer.name.split(' ')[0]}`).should('have.attr', 'alt')
+              .should('contain', developer.name)
+              .get('.dev-title').should('contain', dev.title)
+              .get('.dev-story').should('contain', dev.story)
+          })
+        })
       })
     })
+
+    cy.fixture('spanish.json').as('spanish').then((spanish) => {
+      cy.get('.en-espanol-button').select('Español')
+      cy.fixture('devs.json').as('devData').then((devData) => {
+        const checkAllDevData = devData.map(developer => {
+          const checkAllDevTranslations = spanish.devs.map(dev => {
+            cy.get('.dev-name').should('contain', developer.name)
+              .get(`.${developer.name.split(' ')[0]}`).should('be.visible')
+              .get(`.${developer.name.split(' ')[0]}`).should('have.attr', 'alt')
+              .should('contain', developer.name)
+              .get('.dev-title').should('contain', dev.title)
+              .get('.dev-story').should('contain', dev.story)
+          })
+        })
+      })
+    })
+
+
   });
 
   it('Should have links to each dev\'s LinkedIn and GitHub & the links should open in a new tab', () => {
