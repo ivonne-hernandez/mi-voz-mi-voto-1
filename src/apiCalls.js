a16e4ff4932f24d6b77d21bd9639281d4e7ba50c
Elise Beall <elisebeall@gmail.com>
2022-02-17 10:29:27 -0700

    Remove CORS

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,11 +2,10 @@ import endpoints from './endpoints.js';
 
 export const postNewEmailSubscriber = (newEmailSubscriber) => {
   return fetch(endpoints.addUser, {
-    mode: 'cors',
+    mode: 'no-cors',
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(newEmailSubscriber)
   })
@@ -14,11 +13,10 @@ export const postNewEmailSubscriber = (newEmailSubscriber) => {
 
 export const deleteSubscriber = (email) => {
   return fetch(endpoints.removeUser, {
-    mode: 'cors',
+    mode: 'no-cors',
     method: 'DELETE',
     headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify(email)
   })
