fddc7c536bfd0277dd5f32c1b99757a8c2ee2815
Ivonne Hernandez <ivonne702@gmail.com>
2022-02-26 11:35:09 -0700

    Wrap header elements in a div and add classNames for styling purposes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -11,39 +11,43 @@ const Header = ()  => {
 
   return (
     <header className="header-container">
-      <img
-        className="vote-image"
-        src={voteImage}
-        alt="vote"
-        role="button"
-        focusable="true"
-        onClick={() => navigate('/')} />
-      <Link to="/">
-        <h1 className="app-name">
+      <div className="img-app-name-container">
+        <img
+          className="vote-image"
+          src={voteImage}
+          alt="vote"
+          role="button"
+          focusable="true"
+          onClick={() => navigate('/')} />
+        <Link to="/">
+          <h1 className="app-name">
+            <FormattedMessage
+              id="header.appName"
+              defaultMessage="My Voice, My Vote"
+            />
+          </h1>
+        </Link>
+      </div>
+      <div className="our-story-language-button-container">
+        <button
+          className="our-story-button header-button"
+          onClick={() => navigate('/our-story')}>
           <FormattedMessage
-            id="header.appName"
-            defaultMessage="My Voice, My Vote"
+            id="header.ourStoryButton"
+            defaultMessage="Our Story"
           />
-        </h1>
-      </Link>
-      <button
-        className="our-story-button header-button"
-        onClick={() => navigate('/our-story')}>
-        <FormattedMessage
-          id="header.ourStoryButton"
-          defaultMessage="Our Story"
-        />
-      </button>
-      <select
-        name="language-select"
-        id="language-select"
-        className="en-espanol-button header-button"
-        aria-label="select a language"
-        value={context.locale}
-        onChange={context.selectLanguage}>
-          <option name="english" value="en">English</option>
-          <option name="español" value="es">Español</option>
-      </select>
+        </button>
+        <select
+          name="language-select"
+          id="language-select"
+          className="en-espanol-button header-button"
+          aria-label="select a language"
+          value={context.locale}
+          onChange={context.selectLanguage}>
+            <option name="english" value="en">English</option>
+            <option name="español" value="es">Español</option>
+        </select>
+      </div>
     </header>
   );
 }
